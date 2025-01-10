c4144d21aaa53712f7ba9102b72d245cab932917
pzajas <zajas.piotr@gmail.com>
2025-01-10 12:39:04 +0100

    Change the tabbar color, icons and labels.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,42 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
-import { Platform } from 'react-native'
-
 import { HapticTab } from '@/components/HapticTab'
-import { IconSymbol } from '@/components/ui/IconSymbol'
 import TabBarBackground from '@/components/ui/TabBarBackground'
-import { Colors } from '@/constants/Colors'
-import { useColorScheme } from '@/hooks/useColorScheme'
+import { palette } from '@/src/theme/palette'
+import { Tabs } from 'expo-router'
+import { Platform } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: palette.colors.blue.dark,
+        tabBarInactiveTintColor: palette.colors.white,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute'
+            position: 'absolute',
+            backgroundColor: palette.colors.blue.light
           },
-          default: {}
+          default: {
+            backgroundColor: palette.colors.blue.light
+          }
         })
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />
+          title: 'Colors',
+          tabBarIcon: ({ color }) => <Icon name="color-palette-outline" size={28} color={color} />
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="favourites"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />
+          title: 'Favourites',
+          tabBarIcon: ({ color }) => <Icon name="star-outline" size={28} color={color} />
         }}
       />
     </Tabs>
--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -3,6 +3,10 @@ export const palette = {
     gray: {
       dark: '#333333'
     },
+    blue: {
+      light: '#ADD8E6',
+      dark: '#00008B'
+    },
 
     white: '#FFFFFF',
     black: '#000000',
